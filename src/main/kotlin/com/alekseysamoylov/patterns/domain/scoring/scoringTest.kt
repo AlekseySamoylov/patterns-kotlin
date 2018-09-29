9ac49e8649b03803bb8040624019d2ce331ade09
Aleksey Samoylov <=>
2018-09-29 14:36:12 +0300

    bridge

--- src/main/kotlin/com/alekseysamoylov/patterns/domain/scoring/scoringTest.kt
+++ src/main/kotlin/com/alekseysamoylov/patterns/domain/scoring/scoringTest.kt
@@ -4,5 +4,7 @@ package com.alekseysamoylov.patterns.domain.scoring
 fun main(args: Array<String>) {
   val creditPolicy = UsCreditPolicy(UsScoring())
 
-  println(creditPolicy.getDecision("John"))
+  for (i in 1..100) {
+    println("Decision: " + creditPolicy.getDecision("RandomClient"))
+  }
 }
